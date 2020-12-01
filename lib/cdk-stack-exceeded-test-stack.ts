207ad38d194208ae424ec892afa0aba8aa025e52
mozamimy <alice@mozami.me>
2020-12-01 10:43:17 +0900

    Add a debug code commented out with lambda.Function construct

--- lib/cdk-stack-exceeded-test-stack.ts
+++ lib/cdk-stack-exceeded-test-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 
 import * as iam from '@aws-cdk/aws-iam';
+import * as lambda from '@aws-cdk/aws-lambda';
 import * as lambdaNodejs from '@aws-cdk/aws-lambda-nodejs';
 
 export class CdkStackExceededTestStack extends cdk.Stack {
@@ -15,5 +16,14 @@ export class CdkStackExceededTestStack extends cdk.Stack {
       handler: 'main',
       role: executionRole, // `cdk synth` will run successfully when this line is removed, or we'll get `Maximum call stack size exceeded` error
     });
+
+    // For debug
+    // Following native lambda.Function works well with `role` option
+    // new lambda.Function(this, 'TestFunction', {
+    //   runtime: lambda.Runtime.NODEJS_12_X,
+    //   handler: 'main',
+    //   code: lambda.Code.fromAsset('./lambda'),
+    //   role: executionRole,
+    // });
   }
 }
